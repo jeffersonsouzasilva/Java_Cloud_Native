c70c0e7ec42480a4f600ab20cb339bf728645c21
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-02-24 23:48:33 -0300

    Inicializando repositório

---
+++ 01_Principios de Desenvolvimento de Aplicações/03_Ambiente de Desenvolvimento/02_Escolha sua IDE/novo-projeto-confgithub/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("projeto criado no video de configuracao e vinculacao do vscode com github");
+    }
+}
