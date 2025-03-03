d4d35f0e7ad6352eaea7a3779a455742f274087c
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-03-02 23:26:34 -0300

    Aula Sintaxe com java - Operadores - Atribuição, Aritméticos eUnários

--- 02_Sintaxe Basica com java/01_Aprendendo a Sintaxe Java/operadores/src/Operadores.java
+++ 02_Sintaxe Basica com java/01_Aprendendo a Sintaxe Java/operadores/src/Operadores.java
@@ -20,26 +20,59 @@ public class Operadores {
         double resultado = (10 * 7) + (20/4);
 
         //ATENÇÃO! O operador de adição (+), quando utilizado em variáveis do tipo texto, realizará a “concatenação de textos”.
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+//        String nomeCompleto = "LINGUAGEM" + "JAVA";
+//        System.out.println(nomeCompleto);
 
         // TESTAR EM DEBUG
-        String concatenacao ="?";
-        System.out.println(concatenacao);
+//        String concatenacao ="?";
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1+1+1+"1";
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1+"1"+1+1;
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1+"1"+1+"1";
+//        System.out.println(concatenacao);
+//
+//        concatenacao = "1"+1+1+1;
+//        System.out.println(concatenacao);
+//
+//        concatenacao = "1"+(1+1+1);
+//        System.out.println(concatenacao);
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
+        //Unários
+        /*
+        Esses operadores são aplicados juntamente com um outro
+        operador aritmético. Eles realizam alguns trabalhos básicos
+        como incrementar, decrementar, inverter valores numéricos e
+        booleanos.
 
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
+        (+) Operador unário de valor positivo – números são positivos sem esse operador explicitamente;
+        (-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+        (++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+        (--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+        (!) Operador unário lógico de negação – nega o valor de uma expressão booleana;
+         */
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+        numero = numero * -1;
+        //numero = - numero;  testei assim também também ficou positivo
+        System.out.println(numero);
 
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
+        //atribuir
+        numero ++; //numero = numero + 1;
+        System.out.println(numero);
+        System.out.println(++ numero);
+        System.out.println(-- numero);
+        System.out.println(numero --);
+        System.out.println(numero);
 
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        boolean variavel = true;
+        System.out.println(variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
