0f06358ecf3a7173a46bda9aafeb7b0737948079
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2025-03-03 21:30:10 -0300

    Aula Sintaxe com java - Operadores - Ternarios, Relacionais e Lógicos

--- 02_Sintaxe Basica com java/01_Aprendendo a Sintaxe Java/operadores/src/Operadores.java
+++ 02_Sintaxe Basica com java/01_Aprendendo a Sintaxe Java/operadores/src/Operadores.java
@@ -74,5 +74,117 @@ public class Operadores {
         System.out.println(variavel);
         variavel = !variavel;
         System.out.println(variavel);
+
+        //Ternário
+        int a, b;
+        a = 5;
+        b = 6;
+
+//        String resultadoExerOperaTernario = "";
+//        if(a==b) {
+//            resultadoExerOperaTernario = "verdadeiro";
+//        }else {
+//            resultadoExerOperaTernario = "falso";
+//        }
+        String resultadoExerOperaTernario = a==b ? "verdadeiro":"falso"; // O significado desta linha é a mesma que acima
+
+        System.out.println(resultadoExerOperaTernario);
+
+        //Relacionais
+//        String nomeUm = "Jefferson";
+//        String nomeDois = "Jefferson";
+//        System.out.println(nomeUm == nomeDois);
+
+        String nomeUm = "Jefferson";
+        String nomeDois =new String("Jefferson") ;
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
+
+        /*
+        Comparações avançadas
+        Quando se refere a comparação de conteúdos na linguagem, devemos ter um certo domínio de como o Java trata o armazenamento deste valores na memória.
+
+        Quando estiver mais familiarizado com linguagem, recomendamos se aprofundar no conceito de espaço em memória Stack versus Heap.
+
+        Vamos a alguns exemplos para ilustrar:
+
+        Valor e referência: Precisamos entender que em Java tudo é objeto, logo objetos diferentes podem ter as mesmas características, mas lembrando, são objetos diferentes.
+         */
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+
+        System.out.println(nome1 == nome2); //true
+
+        String nome3 = new String("JAVA");
+
+        System.out.println(nome1 == nome3); //false
+
+        String nome4 = nome3;
+
+        System.out.println(nome3 == nome4); //true
+
+        //equals na parada
+        System.out.println(nome1.equals(nome2)); //??
+        System.out.println(nome2.equals(nome3)); //??
+        System.out.println(nome3.equals(nome4)); //??
+
+        /*
+        == versus equals: Existe uma relevância forte em realizar comparações com == e equals na qual precisamos ter uma compreensão de quais as regras seguidas pela linguagem **** , exemplo:
+         */
+        int numeros1 = 130;
+        int numeros2 = 130;
+        System.out.println(numero1 == numero2); //true
+
+        String numerosi1 = "130";
+        String numerosi2 = new String("130");
+        System.out.println(numerosi1 == numerosi2); //false
+
+        // A razão pela qual o resultado é false, é devido o Java tratar os valores
+        // Como objetos a partir de agora.
+        // Qual a solução ?
+        // Quando queremos comparar objetos, usamos o método equals
+
+        System.out.println(numerosi1.equals(numerosi2));
+
+        //Lógico
+        /*
+        Os operadores lógicos representam o recurso que nos permite criar expressões lógicas maiores a partir da junção de duas ou mais expressões.
+            && Operador Lógico "E"
+            || Operador Lógico "OU"
+         */
+        boolean condicao1=true;
+        boolean condicao2=false;
+        if(condicao1 && condicao2) {
+            System.out.print("Os dois valores precisam ser verdadeiros");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.print("Um dos valores precisa ser verdadeiro");
+        }
+
+        if(condicao1 && (7 > 4)) {
+            System.out.print("Os dois valores precisam ser verdadeiros");
+        }
+        System.out.println("fim");
+
     }
 }
